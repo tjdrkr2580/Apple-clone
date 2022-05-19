4bdd2e31519467ef96be72b86febb96db805af4c
Kim Tae Hyun <97386129+tjdrkr2580@users.noreply.github.com>
2022-05-19 10:17:27 +0900

    현재 활성 씬 반영하기

--- apple-clone-v10/js2/main.js
+++ apple-clone-v10/js2/main.js
@@ -1,4 +1,8 @@
 (()=> {
+
+    let yOffset = 0; // window.pageYOffset 대신 쓸 변수
+    let prevScrollHeight = 0; // 현재 스크롤 위치(yOffset)보다 이전에 위치한 스크롤 섹션들의 스크롤 높이값의 합
+    let currentScene = 0; // 현재 활성화 된(눈 앞에 보고있는) 씬 (scroll-section)
     const sceneInfo = [
         {
             type: 'sticky',
@@ -40,6 +44,27 @@
             sceneInfo[i].objs.container.style.height = `${sceneInfo[i].scrollHeight }px`;
         }
     }
+
+    const scrollloop = () => {
+        prevScrollHeight = 0;
+        for(let i = 0 ; i < currentScene ; i++){
+            prevScrollHeight += sceneInfo[i].scrollHeight;
+        }
+        if(yOffset > prevScrollHeight + sceneInfo[currentScene].scrollHeight){
+            currentScene++;
+        }
+        if(yOffset < prevScrollHeight){
+            if(currentScene === 0) return; //브라우저 바운스 효과로 인해 마이너스가 되는 것을 방지(모바일)
+            currentScene--;
+        }
+        console.log(currentScene);
+    }
+
     window.addEventListener('resize',setLayOut);
+    window.addEventListener('scroll',() => {
+        yOffset = window.pageYOffset;
+        scrollloop();
+    });
+
     setLayOut();
 })();
